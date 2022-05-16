135080ccdfa808caa88cc692b1485401c7991905
Souta10 <103153610+Souta10@users.noreply.github.com>
2022-05-16 12:51:24 +0900

    課題提出テスト1--5
    繰り返し処理

---
+++ main.js
@@ -0,0 +1,10 @@
+function createJuice(fruits) {
+console.log("〇〇を受け取りました。ジュースにして返します")
+    
+
+createJuice("みかん"+"ジュース") 
+
+    return みかんジュース;
+ }   
+    let orangeJuice = createJuice('みかん');
+    console.log=("〇〇が届きました")
